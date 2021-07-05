f3bdbef3ccf602ce58dd113c8f55611ec6a6d876
김민영 <win930323@gmail.com>
2021-07-05 17:37:17 +0900

    project updated

--- sample.js
+++ sample.js
@@ -1,14 +1,14 @@
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const content = document.createElement("div");
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
-const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store = {
   currentPage: 1,
-  feeds:[],
+  feeds: [],
 };
+
 function getData(url) {
-  ajax.open("GET", url, false);
+  ajax.open('GET', url, false);
   ajax.send();
 
   return JSON.parse(ajax.response);
@@ -18,44 +18,43 @@ function makeFeeds(feeds) {
   for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
   }
+
   return feeds;
 }
 
 function newsFeed() {
-  let newsFeed = store.feeds;
+  const newsFeed = store.feeds;
   const newsList = [];
   let template = `
-  <div class="bg-gray-600 min-h-screen">
-  <div class="bg-white text-xl">
-    <div class="mx-auto px-4">
-      <div class="flex justify-between items-center py-6">
-        <div class="flex justify-start">
-          <h1 class="font-extrabold">Hacker News</h1>
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+                Previous
+              </a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
+                Next
+              </a>
+            </div>
+          </div> 
         </div>
-        <div class="items-center justify-end">
-          <a href="#/page/{{__prev_page__}}" class="text-gray-500">
-            Previous
-          </a>
-          <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">
-            Next
-          </a>
-        </div>
-      </div> 
+      </div>
+      <div class="p-4 text-2xl text-gray-700">
+        {{__news_feed__}}        
+      </div>
     </div>
-  </div>
-  <div class="p-4 text-2xl text-gray-700">
-    {{__news_feed__}}        
-  </div>
-</div>
   `;
 
   if (newsFeed.length === 0) {
     newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
   }
-  // newsList.push("<ul>");
 
-  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
-    // const div = document.createElement("div");
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
@@ -75,31 +74,18 @@ function newsFeed() {
         </div>
       </div>    
     `);
-    // ul.appendChild(div.firstElementChild);
   }
 
-  // newsList.push("</ul>");
-  // container.appendChild(ul);
-  // container.appendChild(content);
-  // newsList.push(`
-  //   <div>
-  //     <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
-  //     <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
-  //   </div>
-  // `);
-
-  template = template.replace('{{__news_feed__}}', newsList.join(""));
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
   template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
+  
   container.innerHTML = template;
 }
 
-// const ul = document.createElement("ul");
-
 function newsDetail() {
   const id = location.hash.substr(7);
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
-  // const title = document.createElement("h1");
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -129,8 +115,8 @@ function newsDetail() {
     </div>
   `;
 
-  for (let i = 0; i < store.feeds.length; i++) {
-    if(store.feeds[i].id === Number(id)) {
+  for(let i=0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
       store.feeds[i].read = true;
       break;
     }
@@ -147,30 +133,33 @@ function newsDetail() {
             <strong>${comments[i].user}</strong> ${comments[i].time_ago}
           </div>
           <p class="text-gray-700">${comments[i].content}</p>
-        </div>     
+        </div>      
       `);
+
       if (comments[i].comments.length > 0) {
         commentString.push(makeComment(comments[i].comments, called + 1));
       }
     }
+
     return commentString.join('');
   }
+
   container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 }
 
 function router() {
   const routePath = location.hash;
 
-  if (routePath === "") {
+  if (routePath === '') {
     newsFeed();
-  } else if (routePath.indexOf("#/page/") >= 0) {
+  } else if (routePath.indexOf('#/page/') >= 0) {
     store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
-    newsDetail();
+    newsDetail()
   }
 }
 
-window.addEventListener("hashchange", router);
+window.addEventListener('hashchange', router);
 
 router();
